b1deb665bf866ed6286edb3f4d66268f3ffe1463
PixelPioneers HQ <kagbo19@gmail.com>
2025-07-20 17:18:21 +0000

    About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
 const {darkMode, toggleDarkMode } = useDarkMode();
 
   return (
-    <section id='about' className={${darkMode ? 'dark bg-black' : 'light bg-transparent'} w-full m-auto lg:px-40 px-10 py-20 grid lg:grid-cols-2 grid-cols-1 justify-center items-center gap-10}>
+    <section id='about' className={`${darkMode ? 'dark bg-black' : 'light bg-transparent'} w-full m-auto lg:px-40 px-10 py-20 grid lg:grid-cols-2 grid-cols-1 justify-center items-center gap-10`}>
     <div>
       <img data-aos="zoom-in" src={aboutimg} alt='about image' className='rounded-2xl lg:w-[500px] lg:h-[600px]'/>
     </div>
