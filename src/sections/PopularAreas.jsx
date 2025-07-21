aa667116422fefa121397db81e39f76bcefa1fd0
RaiderGold11 <rgtech391@gmail.com>
2025-07-21 16:51:11 +0000

    fix: Correct wording in Popular Areas section header

--- src/sections/PopularAreas.jsx
+++ src/sections/PopularAreas.jsx
@@ -29,7 +29,7 @@ const {darkMode, toggleDarkMode } = useDarkMode();
             <h1 data-aos="zoom-in" className='text-red-500 dark:text-white'>
               POPULAR AREAS
             </h1>
-            <h1 data-aos="zoom-in" className='text-black text-[40px] font-semibold leading-10 mt-4 dark:text-white'>Explore most<br></br>popular areas</h1>
+            <h1 data-aos="zoom-in" className='text-black text-[40px] font-semibold leading-10 mt-4 dark:text-white'>Explore the most<br></br>popular areas</h1>
           </div>
           <div className='grid lg:grid-cols-3 col-span-2 grid-cols-1 justify-center items-center gap-6'>
             <div data-aos="zoom-in" data-aos-delay="400" style={{backgroundImage: `url(${area1})`}}className=' h-[400px] bg-cover bg-center rounded-xl'></div>
